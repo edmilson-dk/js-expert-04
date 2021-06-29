abe9c9be90d7d7297ecbc42d2d1f004fac9e3110
edmilson-dk <droidvidaboa@gmail.com>
2021-06-28 21:42:42 -0300

    feat: created fake user and room data

---
+++ app/pages/room/src/index.js
@@ -0,0 +1,9 @@
+const room = {
+  id: Date.now(),
+  topic: "Js expert",
+}
+
+const user = {
+  img: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/sloth_lazybones_sluggard_avatar-512.png",
+  username: "Edmilson",
+}
