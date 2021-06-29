b85b1cb1a8b287d39a31f1473ae3bc4229e2ef85
edmilson-dk <droidvidaboa@gmail.com>
2021-06-28 21:48:54 -0300

    feat: created socket builder instance

--- app/pages/room/src/index.js
+++ app/pages/room/src/index.js
@@ -1,3 +1,16 @@
+import { constants } from "../../_shared/constants.js";
+import SocketBuilder from "../../_shared/socketBuilder.js";
+
+const socketBuilder = new SocketBuilder({
+  socketUrl: constants.socketUrl,
+  namespace: constants.socketNamespaces.room,
+});
+
+const socket = socketBuilder
+  .setOnUserConnected((user) => console.log("User connected: ", user))
+  .setOnUserDisconnected((user) => console.log("User disconnected: ", user))
+  .build();
+
 const room = {
   id: Date.now(),
   topic: "Js expert",
@@ -7,3 +20,5 @@ const user = {
   img: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/sloth_lazybones_sluggard_avatar-512.png",
   username: "Edmilson",
 }
+
+socket.emit(constants.events.JOIN_ROOM, { user, room });
