ae084cae4363cdda00689a7d8cfbf9cdbdb4af87
edmilson-dk <droidvidaboa@gmail.com>
2021-06-29 18:45:20 -0300

    feat: created rooms controller

---
+++ server/src/controllers/roomsController.js
@@ -0,0 +1,19 @@
+export default class RoomsController {
+  constructor() {
+
+  }
+
+  onNewConnection(socket) {
+    const { id } = socket;
+    console.log("connection stablished with #",id);
+  }
+
+  getEvents() {
+    const functions = Reflect.ownKeys(RoomsController.prototype)
+      .filter(fn => fn !== 'constructor')
+      .map(name => [name, this[name].bind(this)])
+
+    console.log("FUNCTIONS: ",functions);
+    return new Map(functions);
+  }
+}
