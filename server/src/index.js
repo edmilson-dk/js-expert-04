7a7d499ee5e2c78d6bb0551a05024b6eb5bca12c
edmilson-dk <droidvidaboa@gmail.com>
2021-06-29 18:46:43 -0300

    feat: added rooms controller

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,34 @@
+import Events from "events";
+
+import RoomsController from "./controllers/roomsController.js";
+import { constants } from "./util/constants.js";
 import SocketServer from "./util/socket.js";
 
 const PORT = process.env.PORT || 3003;
 
 const socketServer = new SocketServer({ port: PORT });
 const server = await socketServer.start();
+
+const roomsController = new RoomsController();
+
+const namespaces = {
+  room: { 
+    controller: roomsController, 
+    eventEmitter: new Events(),
+  }
+}
+
+const routeConfig = Object.entries(namespaces)
+  .map(([namespace, { controller, eventEmitter }]) => {
+    const controllerEvents = controller.getEvents();
+    eventEmitter.on(
+      constants.events.USER_CONNECTED,
+      controller.onNewConnection.bind(controller)
+    )
+
+    return {
+      [namespace]: { events: controllerEvents, eventEmitter }
+    }
+  })
 
 console.log("Socket server is running at", server.address().port);
