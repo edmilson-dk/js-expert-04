48d8eea1a2065975a74ce8ac4e2da7a61d8fb5c7
edmilson-dk <droidvidaboa@gmail.com>
2021-06-30 21:48:28 -0300

    feat: created update global user data

--- server/src/controllers/roomsController.js
+++ server/src/controllers/roomsController.js
@@ -1,16 +1,47 @@
+import Attendee from "../entities/attendee.js";
 import { constants } from "../util/constants.js";
 
 export default class RoomsController {
-  constructor() {}
+  #users = new Map();
+
+  constructor() {
+    this.rooms = new Map();
+  }
+
+  #updateGlobalUserData(userId, userData = {}, roomId = "") {
+    const user = this.#users.get(userId) ?? {};
+    const existingRoom = this.rooms.has(roomId);
+
+    const updatedUserData = new Attendee({
+      ...user,
+      ...userData,
+      roomId,
+      isSpeaker: !existingRoom
+    });
+
+    this.#users.set(userId, updatedUserData);
+
+    return this.#users.get(userId);
+  }
 
   onNewConnection(socket) {
     const { id } = socket;
     console.log("connection stablished with #", id);
+    this.#updateGlobalUserData(id);
   }
 
-  joinRoom(socket, data) {
-    console.log('dados recebidos', data);
-    socket.emit(constants.events.USER_CONNECTED, data);
+  joinRoom(socket, { user, room }) {
+    const userId = user.id = socket.id;
+    const roomId = room.id;
+
+    const updatedUserData = this.#updateGlobalUserData(
+      userId,
+      user,
+      roomId
+    );
+
+    console.log({ updatedUserData });
+    socket.emit(constants.events.USER_CONNECTED, updatedUserData);
   }
 
   getEvents() {
